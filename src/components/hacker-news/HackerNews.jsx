9befaa1cd57d611f9f72cc2cf7219d006f04d1ca
Alex <alex.mehan@hotmail.com>
2025-06-26 09:17:50 +1000

    Retrieved and displayed articles and styled

--- src/components/hacker-news/HackerNews.jsx
+++ src/components/hacker-news/HackerNews.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 export default function HackerNews() {
     const [articles, setArticles] = useState([])
     useEffect(() => {
-        fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$priority"&limitToFirst=10')
+        fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$priority"&limitToFirst=12')
             .then((response) => {
                 if (!response.ok) {
                     throw new Error("Could not get articles")
@@ -20,29 +20,32 @@ export default function HackerNews() {
                             return response.json()
                         })
                         .then((jsonData)  => {
-                            // console.log(jsonData)
                             setArticles(prev => [...prev, jsonData])
                         })
                         .catch((err) => {
                             console.log(err)
                         })
                 })
-                // setArticles(jsonData)
             })
             .catch((err) => {
                 console.log(err)
             })
     }, [])
-
-    console.log(articles)
-
-    useEffect(() => {
-        
-    }, [])
+    const articleElements = articles.map((article) => (
+        <div className="shadow-md rounded-md p-8 flex justify-center flex-col flex-wrap border border-gray-300">
+            <h3 className="text-xl font-weight-500 text-indigo-500"><a href={article.url}>{article.title}</a></h3>
+            <div>
+                <span>Score: {article.score}</span> <span>by {article.by}</span>
+            </div>
+        </div>
+    ))
 
     return (
         <section className="py-8 border-b border-black">
             <h2 className="font-extrabold text-4xl mb-4 text-center">Top 10 Articles - Hacker News</h2>
+            <div className="grid grid-cols-4 gap-6">
+                {articleElements}
+            </div>
         </section>
     )
 }
